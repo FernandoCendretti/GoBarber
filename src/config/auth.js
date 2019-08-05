a8c674dbf9a2d815ea23da4304242a6005f0323c
FernandoCendretti <luiz.cendretti@gmail.com>
2019-08-05 20:04:00 -0300

    Criação , Atualização e Validação do usuário

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '363fa5f030d7f336f4cab00eb7b08c85',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,18 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+// everything below that line must pass the Middleware Authentication
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 export default routes;
