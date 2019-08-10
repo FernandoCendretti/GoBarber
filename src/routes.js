ea72aa8b70bce8610529bb3006b815b461e8235f
FernandoCendretti <luiz.cendretti@gmail.com>
2019-08-10 15:40:09 -0300

    Agendamento de Serviço

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentsController from './app/controllers/AppointmentsController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -21,6 +22,8 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentsController.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
